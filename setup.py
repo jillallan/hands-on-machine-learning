a0834a24506b9d665ea0ca46747c59b9dbd8a9e5
Jill Allan <jillallan24@gmail.com>
2022-11-15 10:02:30 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementation of the Hands on machine learning book, organised as a cookiecutter template',
+    author='Jill Allan',
+    license='MIT',
+)
